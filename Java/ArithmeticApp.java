f45953cdf91059c6fe5a8c23fa3020c2e68d8228
Loba007 <paulacious4greatness@gmail.com>
2025-12-08 16:29:24 +0100

    save my changes

---
+++ Java/ArithmeticApp.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.math.random;
+public class ArithmeticApp{
+   public static void main(String[] args){
+Scanner scanner = new Scanner(System.in);
+System.out.println("5 - 3 = ?");
+ int  answer = 2;
+String input  = scanner.nextLine();
+
+if (answer = 2){
+System.out.println("Correct!");
+}
+else (answer != 2){ 
+System.out.println("Wrong! Try again for the second time.");
+}
+
+//time.time
+//random random = new random();
+
+//System.out.println("20 - 10 = ?");
+int numberTwo = 10;
+System.out.println("5 - 4 = ?");
+int numberThree = 1;
+System.out.println("63- 3 = ?");
+int nuberrFour = 60;
+System.out.println("8 - 4 = ?");
+int numberFive = 4;
+System.out.println("56- 40 = ?");
+int numberSix = 16;
+System.out.println("26 - 3 = ?");
+int numberSeven = 23;
+System.out.println("35 - 20 = ?");
+int numberEight = 15;
+System.out.println("40 - 20 = ?");
+int numberNine = 20;
+System.out.println("60 - 30 = ?");
+int numberTen = 30;
+
+
+
+
+
+}
+
+
+
+
+
+}
---
+++ Python/arithmethic_app.py
@@ -0,0 +1,37 @@
+
+print('Answer these random 10 subtraction problem questions:')
+number1 = int(input('5 - 4:'))
+answer1 = 1
+
+if (answer1 == 1):
+    print('Correct!')
+
+if (answer1 != 1):
+    print('Wrong! Try again for the second time.')
+
+if (answer1 != 1):
+    print('Wrong again! Move to the next question.')
+
+
+number2 = int(input('8 - 5:'))
+
+answer2 = 3
+
+if (answer2 == 3):
+    print('Correct!')
+
+if (answer1 != 1):
+    print('Wrong! Try again for the second time.')
+
+if (answer1 != 1):
+    print('Wrong again! Move to the next question.')
+#if (answer_One != 2)
+#print(Wrong! Try again the second time.)
+
+#elif (answer_One != 2)
+#print(Wrong! Move to the next question.) 
+
+
+#
+
+
