37a1dc9d2dd3f6c6b6fa3a551341f8c720f4f8a6
Loba007 <paulacious4greatness@gmail.com>
2025-12-09 11:45:32 +0100

    Added Java, JavaScript, and Python folders with files

---
+++ Java/GroceryManagerApp.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class GroceryManagerApp{
+    public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.println("\nWELCOME TO GROCERY MANAGER APP!");
+System.out.println("Kindly enter the list of items to be added for purchase:");
+String userInput = scanner.nextLine();
+
+int addItem = [0];
+String 
+
+     System.out.println("""
+    
+     ALL AVAILABLE ITEMS IN THE STORE:
+
+     1. Rice
+     2. Toothpaste
+     3. Cornflakes
+     4. BoKku Bread
+     5. Egg
+     6. Yam Powder
+     7. Vegetables
+     8. GoldenMorn
+     9. Beans
+     10.Yoghurt
+     11. Milk
+
+     """);
+
+
+
+
+//String [] itemList = {""};
+//String [] itemlist = {"Rice, Beans, Toothpaste, Vegetables, Yam, Soda, Yoghurt, Cornflakes, GoldenMorn"};
+
+
+
+     }
+
+   }
---
+++ Python/grocery_manager_app.py
@@ -0,0 +1,38 @@
+
+print("-------------------------------")
+print("WELCOME TO GROCERY MANAGER APP!")
+print("-------------------------------")
+
+user_input = input("Kindly enter the list of items to be added for purchase:")
+
+grocery_list = (input("""
+
+     AVAILABLE ITEMS IN THE STORE
+
+     1. Rice
+     2. Toothpaste
+     3. Cornflakes
+     4. BoKku Bread
+     5. Egg
+     6. Yam Powder
+     7. Vegetables
+     8. GoldenMorn
+     9. Beans
+     10.Yoghurt
+     11. Milk
+                       """))
+
+def add_item(input("Groundnut oil, Salt, Tomato Pastes"));
+
+ 
+
+
+
+
+
+
+
+
+
+
+
